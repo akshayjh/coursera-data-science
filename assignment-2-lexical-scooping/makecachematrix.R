276d6667922b2371525bda7f1345e252afbea5dd
bharathgs <royalkingpin@gmail.com>
2015-09-25 20:08:48 +0530

    added some comments

--- assignment-2-lexical-scooping/makecachematrix.R
+++ assignment-2-lexical-scooping/makecachematrix.R
@@ -1,3 +1,4 @@
+#vmakeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     set <- function(y) {
@@ -12,6 +13,9 @@ makeCacheMatrix <- function(x = matrix()) {
          setInverse = setInverse,
          getInverse = getInverse)
 }
+
+#cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated, 
+#then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
     inv <- x$getInverse()
